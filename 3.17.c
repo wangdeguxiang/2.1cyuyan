c88f7faf1a22e1320d1b7ce6e17b757f5122785f
wangdeguxiang <514435906@qq.com>
2023-03-17 23:03:19 +0800

    3.17

---
+++ 3.17.c
@@ -0,0 +1,41 @@
+
+int main(){
+    int n = 0;
+    scanf("%d",&n);
+    int i = 0;
+    int ret = 1;
+    int sum = 0;
+    int j = 0;
+    for(j = 1;j <= n;j++){
+        ret = 1;
+        for(i = 1;i <= j; i++){
+            ret * =i;
+        }
+        sum +=ret;
+    }
+    printf("%d\n",sum);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+    int i = 0;
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    for(i = 0; i <= 9;i++){
+        arr[i] = 0;
+        printf("hehe\n");
+    }
+    return 0;                                          
+}
