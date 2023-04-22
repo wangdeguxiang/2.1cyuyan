551e6c1be38062b4d0387da3a9fe3a70b011803b
wangdeguxiang <514435906@qq.com>
2023-04-23 00:08:59 +0800

    4.22

---
+++ 4.22.c
@@ -0,0 +1,19 @@
+int main(){
+    char *a[]={"work","at","aaaa"};
+    char **pa=a;
+    printf("%s\n",*pa);
+    return 0;
+    }
+
+
+int main(){
+    char * []={"exet","new","point","float"};
+    char**cp[]{c+3,c+2,c+1,c};
+    char **cpp=cp
+    printf("%S\n",**++cpp);
+    printf("%S\n",*--*++cpp+3);
+    printf("%S\n",*cpp[-2]+3);
+    printf("%S\n",cpp[-1][-1]+1);
+    return 0;
+    
+}
